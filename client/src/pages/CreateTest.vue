cc469efe95ac2cbac51fabbf5552a17284848094
Alika <Alikyshkin@gmail.com>
2023-12-10 22:14:06 +0300

    fix: создание и список тестов

--- client/src/pages/CreateTest.vue
+++ client/src/pages/CreateTest.vue
@@ -18,41 +18,54 @@
     <!-- Вопросы -->
     <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <div v-for="(question, qIndex) in test.questions" :key="qIndex" class="mb-4">
+        <input
+            class="form-input mb-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text" placeholder="Название вопроса" v-model="question.title">
         <textarea
-            class="form-textarea shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Текст вопроса" v-model="question.text"></textarea>
-
+            class="form-textarea mb-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Содержание вопроса" v-model="question.content"></textarea>
         <!-- Выбор типа вопроса -->
         <select v-model="question.type"
-                class="form-select block w-full mt-1 shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-          <option value="text">Короткий ответ</option>
-          <option value="single">Одиночный выбор</option>
-          <option value="multiple">Множественный выбор</option>
+                class="form-select block w-full mt-1 mb-3 shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <option value="short-answer">Короткий ответ</option>
+          <option value="single-choice">Одиночный выбор</option>
+          <option value="multiple-choice">Множественный выбор</option>
         </select>
-
+        <!-- Для коротких ответов -->
+        <div v-if="question.type === 'short-answer'">
+          <input
+              class="form-input mb-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text" placeholder="Правильный ответ" v-model="question.answer">
+        </div>
         <!-- Варианты ответа -->
-        <div v-if="question.type === 'single' || question.type === 'multiple'">
-          <div v-for="(answer, aIndex) in question.answers" :key="aIndex" class="flex">
-            <input class="form-input w-full block mt-1 shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" v-model="answer.text">
-            <button @click="removeAnswer(qIndex, aIndex)"
-                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        <div v-if="question.type === 'single-choice' || question.type === 'multiple-choice'">
+          <div v-for="(option, oIndex) in question.options" :key="oIndex" class="flex items-center mb-2">
+            <input
+                class="form-input w-full block shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text" v-model="option.text">
+            <input
+                class="ml-2 form-checkbox h-5 w-5 text-gray-600"
+                type="checkbox"
+                v-model="option.isCorrect">
+            <button @click="removeOption(qIndex, oIndex)"
+                    class="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
               Удалить
             </button>
           </div>
-          <button @click="addAnswer(qIndex)"
+          <button @click="addOption(qIndex)"
                   class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 w-full">
             Добавить ответ
           </button>
         </div>
 
-        <button @click="removeQuestion(index)"
+        <button @click="removeQuestion(qIndex)"
                 class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 w-full">
-          <i class="fas fa-minus"></i>
+          Удалить вопрос
         </button>
       </div>
     </div>
     <button @click="addQuestion" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
-      Добавить вопрос <i class="fas fa-plus"></i>
+      Добавить вопрос
     </button>
     <button @click="submitTest" class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded w-full mt-4">
       Создать тест
@@ -77,20 +90,24 @@ export default {
   methods: {
     addQuestion() {
       this.test.questions.push({
-        text: '',
-        type: 'text', // Установка начального значения типа вопроса
-        answers: [],
+        title: '',
+        content: '',
+        type: 'short-answer',
+        answer: '', // Для коротких ответов
+        options: [], // Для вопросов с выбором
       });
     },
-    removeQuestion(index) {
-      this.test.questions.splice(index, 1);
+    removeQuestion(qIndex) {
+      this.test.questions.splice(qIndex, 1);
     },
-    addAnswer(questionIndex) {
-      this.test.questions[questionIndex].answers.push({text: ''});
+    addOption(qIndex) {
+      this.test.questions[qIndex].options.push({
+        text: '',
+        isCorrect: false
+      });
     },
-
-    removeAnswer(questionIndex, answerIndex) {
-      this.test.questions[questionIndex].answers.splice(answerIndex, 1);
+    removeOption(qIndex, oIndex) {
+      this.test.questions[qIndex].options.splice(oIndex, 1);
     },
     submitTest() {
       fetch('https://example.com/api/tests', { // Замените URL на фактический URL вашего API
@@ -99,7 +116,7 @@ export default {
           'Content-Type': 'application/json',
           // Добавьте другие заголовки, если необходимо, например, для аутентификации
         },
-        body: JSON.stringify(this.test),
+        body: JSON.stringify(this.prepareTestData()),
       })
           .then(response => {
             if (!response.ok) {
@@ -114,6 +131,23 @@ export default {
           .catch(error => {
             console.error('Ошибка при отправке теста:', error);
           });
+    },
+    prepareTestData() {
+      // Преобразование данных теста в формат, ожидаемый сервером
+      const preparedTest = { ...this.test };
+      preparedTest.questions = preparedTest.questions.map(question => {
+        if (question.type === 'single-choice' || question.type === 'multiple-choice') {
+          return {
+            ...question,
+            options: question.options.map(option => ({
+              text: option.text,
+              isCorrect: option.isCorrect,
+            })),
+          };
+        }
+        return question;
+      });
+      return preparedTest;
     }
   }
 };
